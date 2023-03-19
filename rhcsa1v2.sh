cccb979fe3e377f2978c47f63c18b4539bbc4f5b
Furkan Budak <furkan.budak@otmail.de>
2023-03-19 17:43:25 +0100

    Das ist mein zweiter vesuch bei der ersten RHCSA Practice-Examen Shellscript Aufgabe

---
+++ rhcsa1v2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+NUMBER=555
+for COUNT in {1..3}
+do
+	/usr/sbin/useradd user$NUMBER -s /sbin/nologin
+	echo "user$NUMBER" | /usr/bin/passwd --stdin user$NUMBER
+	/usr/bin/cat /etc/passwd | grep user$NUMBER >> /var/tmp/newusers
+	NUMBER=$((NUMBER+111))
+done
+exit
+		
